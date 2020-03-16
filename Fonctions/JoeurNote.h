25f8d3cbc52af0440eec5b12a83179ea30393dc4
danlux18 <57712212+danlux18@users.noreply.github.com>
2020-03-16 15:25:40 +0100

    Create JoeurNote.h

---
+++ Fonctions/JoeurNote.h
@@ -0,0 +1,16 @@
+#ifndef JouerNote_h
+#define JouerNote_h
+    
+#include <Arduino.h>
+#include <Servo.h>
+
+
+void JoueNote(int ID);
+void Initialisation();
+void JingleBells();
+void GameOfThrones();
+void BrilleBrille();
+void AuClairDeLaLune();
+void SmokeOnWater();
+void Aristochats();
+#endif
